8890388f9e45a3e6a4c537ab7d7b83dffaaef984
Petar <pebe1928@gmail.com>
2024-12-13 12:07:09 +0100

    Updates

--- _next/static/chunks/app/en/page-06e51c23b9d8141c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[496],{5820:(e,s,n)=>{Promise.resolve().then(n.bind(n,7536)),Promise.resolve().then(n.bind(n,7397)),Promise.resolve().then(n.bind(n,305)),Promise.resolve().then(n.bind(n,8419)),Promise.resolve().then(n.bind(n,5561))}},e=>{var s=s=>e(e.s=s);e.O(0,[565,5,441,517,358],()=>s(5820)),_N_E=e.O()}]);
